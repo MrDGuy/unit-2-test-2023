d2b59ffc1731c680dfcc1d34d818a9071d311e6e
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-09-26 10:07:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-points = game.ask_for_number("Points per game?")
-rebounds = game.ask_for_number("Rebounds per game?")
-assists = game.ask_for_number("Assists per game?")
-if points >= 25 or points >= 10 and rebounds >= 10 and assists >= 10:
-    adventure.add_to_textlog("You are an all-star!")
-else:
-    adventure.add_to_textlog("Keep Trying!")
+
